a16df3f2541e4f42d56c7d010f4387c04cf506cc
madhur acharya <40020328+madhur-acharya@users.noreply.github.com>
2019-06-25 21:55:50 +0530

    Add files via upload

---
+++ routers/signup.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const path= require('path');
+const bodyParser= require('body-parser');
+const https = require('https');
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client('187100638059-bmdb2o84d54j32lqmn1r4kh3prg48s5e.apps.googleusercontent.com');
+
+const databaseSystem= require(path.join(__dirname, '../lib/data.js'));
+const utility= require(path.join(__dirname, '../lib/utility-functions.js'));
+
+var router = express.Router();
+var database= new databaseSystem();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var JSON_parser= bodyParser.json();
+
+router.get('/', (request, response) => response.sendFile(path.join(__dirname, '../public/signup/signup.html')));
+
+router.post('/', JSON_parser, function(request, response)
+{
+
+	let user_data= {};
+
+	if(request.body.validationType === "google")
+	{
+		user_data= {
+			email: request.body.email,
+			name: request.body.name,
+			image: request.body.image,
+			id: request.body.id,
+			validationType: request.body.validationType,
+		}
+	}
+	else if(request.body.validationType === "github")
+	{
+		user_data= {
+			email: request.body.email,
+			name: request.body.name,
+			image: request.body.image,
+			id: request.body.id,
+			validationType: request.body.validationType,
+		}
+	}
+	else
+	{
+		user_data= {
+			email: request.body.email,
+			name: request.body.name,
+			validationType: request.body.validationType,
+			password: utility.hash(request.body.password)
+		}
+	}
+
+	database.create(path.join(__dirname, "../data/" + request.body.validationType + '/' + user_data.email + ".json"), JSON.stringify(user_data), err => {
+		console.log(err.errno == -4075 ? "this account is already registered" : !err ? "created new user" + user_data.name : err);
+		
+		if(err) 
+		{
+			if(err.errno == -4075 ) 
+			{
+				response.send(user_data.validationType == 'github' ? '<script>window.alert("User already exists, please login"); window.location.href= location.origin + "/login";</script>'
+				: JSON.stringify({"action" : "redirect", "value" : '/login'}));
+			}
+			else
+			{
+				console.log('\x1b[31m%s\x1b[0m', err);
+				response.send(JSON.stringify({"action" : "error", "message" : 'Whoops. please try again', "value" : "/signup"}));
+			}
+		}
+		else
+		{
+			let token= utility.manage_tokens('POST', '/token', result => {
+				result= JSON.parse(result);
+				if(result.action == 'authenticate')
+				{
+					response.send(user_data.validationType == 'github' ? '<script>window.alert("signed up sucessfully!"); localStorage.setItem("NerdTalk_authorization", ' + "'" +  result.token + "'" + ' ); window.location.href= location.origin + "/dashboard";</script>' 
+						: JSON.stringify({"action" : "create_session", "token" : result.token}));
+				}
+				else
+				{
+					console.log('\x1b[31m%s\x1b[0m', result);
+					response.send(user_data.validationType == 'github' ? '<script>window.alert("unable to generate token. please try again"); window.location.href= location.origin + "/signup";</script>' 
+							: JSON.stringify({"action" : "error", "value" : 'signup', 'message' : "unable to generate token. please try again"}));
+				}
+			}, JSON.stringify(user_data));
+		}
+	});
+});
+
+
+module.exports= router; 
---
+++ server.js
@@ -0,0 +1,41 @@
+const express= require('express');
+var path= require('path');
+var app= new express();
+
+const databaseSystem= require(path.join(__dirname, '/lib/data.js'));
+const utility= require(path.join(__dirname, '/lib/utility-functions.js'));
+var database= new databaseSystem();
+
+app.use(express.static('public'));
+
+//routes-----------------------------------------------------------------------------------------------------
+app.get('/', function(request, response)
+{
+	response.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+var login_handler= require('./routers/login.js');
+app.use('/login', login_handler);
+
+var signup_handler= require('./routers/signup.js');
+app.use('/signup', signup_handler);
+
+var OAuth_handler= require('./routers/OAuth.js');
+app.use('/OAuth', OAuth_handler);
+
+var tokens_handler= require('./routers/token.js');
+app.use('/token', tokens_handler);
+
+var file_test= require('./routers/datatest.js');
+app.use('/test', file_test);
+
+var dashboard_handler= require('./routers/dashboard.js');
+app.use('/dashboard', dashboard_handler);
+
+app.get('*', (request, response) => response.send('whoops'));
+app.get('/favicon.ico', (request, response) => response.send('#'));
+
+let interval= setInterval(() => utility.clear_tokens(result => {if(result) console.log("tokens removed!")}),60000);
+
+app.listen(3000, err => console.log( err ? err : "listening on port: 3000"));
+//--
